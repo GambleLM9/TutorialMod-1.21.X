712f1c2cbca65212d545d6bc61cd046fa52830d2
GambleLM9 <profgyr0@gmail.com>
2026-05-03 13:26:59 -0700

    added testcommit

--- src/main/java/net/swallow/tutorialmod/TutorialMod.java
+++ src/main/java/net/swallow/tutorialmod/TutorialMod.java
@@ -4,7 +4,7 @@ import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+// commeenty
 public class TutorialMod implements ModInitializer {
 	public static final String MOD_ID = "tutorialmod";
 	// This logger is used to write text to the console and the log file.
